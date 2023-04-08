1fe02da5fbead436e8fd79205dbe95e5da78fe55
Zhannur <221952@astanait.edu.kz>
2023-04-09 03:31:33 +0600

    implementetion of func wich finds min from array

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {10, 1, 32, 3, 45};
+        int min = findMininum(arr, arr.length);
+        System.out.println("Minimum in the array is: " + min);
+    }
+
+    public static int findMininum(int[] arr, int n) {
+        if (n == 1) {
+            return arr[0];
+        } else {
+            int minValue = findMininum(arr, n - 1);
+            return Math.min(minValue, arr[n - 1]);
+        }
+    }
+}
+/*
+@ findMininum -definition of min
+@ arr - array
+@ n - lenght of array
+@return - returning minimum of function
+ */
