8f1f1ce5683b1521158e821a621ce13723d93939
Zhannur <221952@astanait.edu.kz>
2023-04-09 15:38:29 +0600

    Fibonacci sequence using recursion

---
+++ src/Fibonacci.java
@@ -0,0 +1,23 @@
+//5
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = 17;
+        int fn = fibonacci(n);
+        System.out.println(fn);
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {  // 1: F0 = 0
+            return 0;
+        } else if (n == 1) {  // 2: F1 = 1
+            return 1;
+        } else {  // Fn = Fn-1 + Fn-2
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
+    }
+}
+/*
+@ fibonacci - definition of fibonacci
+@ n- length
+@ return - returning fibonacci of function
+ */
