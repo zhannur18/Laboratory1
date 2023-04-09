480cc726461371db233ff4d65c0dccaa04550555
Zhannur <221952@astanait.edu.kz>
2023-04-09 15:32:58 +0600

    find the factorial of a number

---
+++ src/Factorial.java
@@ -0,0 +1,20 @@
+// 4
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 5;
+        double sum = factorial(n);
+        System.out.println(sum);
+    }
+    public static int factorial(int n){
+        if(n == 0){
+            return 1;
+        }else{
+            return n * factorial(n-1) ;
+        }
+    }
+}
+/*
+@ factorial - definition of factorial
+@ n- length of array
+@ return - returning factorial of function
+ */
